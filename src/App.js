da016db94c7fc5ce8506efafe53d4a5e16f30dc1
Migue Cabrera <mcabrera@minutentag.com>
2018-09-19 11:40:21 -0300

    rebuild pokedex structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from "react";
-import Header from "./components/Header";
-import Pokedex from "./components/Pokedex";
-import "bulma/css/bulma.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-// redux
-import { Provider } from 'react-redux';
-import store from './store';
-
-const App = () => (
-  <Provider store={store}>
-    <div>
-      <Header />
-      <Pokedex />
-    </div>
-  </Provider>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import pokedexReducer from './pokedexReducer';
-
-export default combineReducers({
-  pokedex: pokedexReducer
-});
